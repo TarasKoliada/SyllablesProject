060b278aba3cee69a5169fe7f7ccf4948c2802c7
Taras <kolyada.taras42@gmail.com>
2024-04-17 22:36:40 +0300

    Some improvements

--- Syllables_master/Core/TextProcessors/Transcribers/AncientTranscriber.cs
+++ Syllables_master/Core/TextProcessors/Transcribers/AncientTranscriber.cs
@@ -8,8 +8,25 @@ namespace Core.TextProcessors.Transcribers
 {
     public class AncientTranscriber : ISpellingTranscriber
     {
+        private readonly Dictionary<string, string> charsToReplace;
+        public AncientTranscriber()
+        {
+            charsToReplace = new Dictionary<string, string>
+            {
+                {"и", "і"},
+                {"е", "jе"},
+                {"ю", "jу"},
+                {"я", "jа"},
+                {"ы", "и"}
+            };
+        }
         public string Transcribe(string text)
         {
+            /*var currentWord = new StringBuilder(text);
+
+            foreach (var charsPair in charsToReplace)
+                currentWord.Replace(charsPair.Key, charsPair.Value);*/
+
             return text;
         }
     }
